9ac8dbfc843c9c78f89245dbb1a78fa112761db2
Graham P Heath <graham.heath@rga.com>
2013-08-14 16:18:23 -0500

    Card API returning fun

--- API/getCard.php
+++ API/getCard.php
@@ -2,8 +2,12 @@
 
 header('Content-Type: application/json');
 
-$results = array("Yellow killer whale","Smarty Lear's Macaw");
-$execString = "./succubusFun.rb "."3" ;
+$results = array();
+if($_GET['seed']&&$_GET['seed']!=0){
+  $execString = "./succubusFun.rb ".$_GET['seed'] ;
+}else{
+  $execString = "./succubusFun.rb";
+}
 $results[]= exec($execString);
 echo json_encode($results);
 
